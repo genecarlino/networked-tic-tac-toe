b21a118a2ce6ad6df667045dea4138503bc3592c
genecarlino <carlino.eu@northeastern.edu>
2023-04-17 17:02:15 -0400

    ttt switcvhed up

--- ttt.py
+++ ttt.py
@@ -1,165 +1,173 @@
 import sys
 import socket
 import argparse
-from typing import Tuple
-import os
-import random
+from enum import Enum
+#from typing import Tuple
 
-# Constants
+#Default Port to be used if no port is specified
 DEFAULT_PORT = 5131
-BUFFER_SIZE = 1024
 
-# Function to print the Tic Tac Toe board to the console
-def print_board(board: str) -> None:
-    for i in range(0, 9, 3):
-        # Print each row of the board
-        print(" ".join(board[i:i + 3]))
-        if i < 6:
-            # Print horizontal dividers between rows
-            print("-----")
+#Enumeration for the two players
+class Player(Enum):
+    X = 'X'
+    O = 'O'
 
-# Function to check if there is a winner in the current board state
-def check_winner(board: str) -> str:
-    if not board:  # line to check if the board is empty
-        return ""
-    win_positions = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
-                     (0, 3, 6), (1, 4, 7), (2, 5, 8),
-                     (0, 4, 8), (2, 4, 6)]
-    for a, b, c in win_positions:
-        if board[a] == board[b] == board[c] and board[a] != " ":
-            return board[a]
-    return ""
+#Class to represent the board
+class Board:
+    def __init__(self):
+        #Initialize the board to be empty, 3x3
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+    # Method to make a move on the board
+    def make_move(self, row: int, col: int, player: Player) -> bool:
+        if self.board[row][col] == ' ':
+            self.board[row][col] = player.value
+            return True
+        return False
+    # Method to check if a player has won
+    def is_winner(self, player: Player) -> bool:
+        for i in range(3):
+            if all(self.board[i][j] == player.value for j in range(3)) or all(self.board[j][i] == player.value for j in range(3)):
+                return True
 
+        if all(self.board[i][i] == player.value for i in range(3)) or all(self.board[i][2 - i] == player.value for i in range(3)):
+            return True
 
-# Function to handle a player's move, update the board, and check for a winner
-def handle_move(board: str, player: str, move: int) -> Tuple[str, bool]:
-    # Check if the move is valid - space is empty and within the board
-    if 0 <= move < 9 and board[move] == " ":
-        # Update the board with the player's move
-        board = board[:move] + player + board[move + 1:]
-        # Check if the move resulted in a win
-        winner = check_winner(board)
-        if winner:
-            # Print the updated board and a winning message
-            print_board(board)
-            print(f"Player {player} wins!")
-            return board, True  # Return the updated board and a flag indicating the game is over
-        return board, False  # Return the updated board and a flag indicating the game is not over
-    else:
-        # Print an error message for an invalid move
-        print("Invalid move. Try again.")
-        return board, False  # Return the unchanged board and a flag indicating the game is not over
+        return False
+    # Method to check if the board is full
+    def is_full(self) -> bool:
+        return all(self.board[i][j] != ' ' for i in range(3) for j in range(3))
+    # Method to return a string representation of the board
+    def __str__(self) -> str:
+        return '\n'.join([' '.join(self.board[i]) for i in range(3)])
 
-
-def server(port: int) -> None:
+#Server function - sets up server, listens for client conn 
+def server(port: int):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("", port))
+        s.bind(('', port))
         s.listen(1)
-        print(f"Server ready. Listening on port {port}.")
+        print(f"Server started on port {port}, waiting for a client...")
         conn, addr = s.accept()
         with conn:
-            print(f"Connected by {addr}")
+            print(f"Client connected from {addr}")
+            play_game(conn, True)
 
-            # Choose a symbol for the server and send the correct symbol to the client
-            server_symbol = random.choice(["X", "O"])
-            client_symbol = "O" if server_symbol == "X" else "X"
-            conn.sendall(client_symbol.encode())  # Send the client symbol to the client
+#client function that connects to a server
+def client(host: str, port: int):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        print(f"Connected to server {host} on port {port}")
+        play_game(s, False)
 
-            replay = "yes"
-            while replay.lower() == "yes":
-                board = " " * 9  # Reset the board at the beginning of each game
-                game_over = False
-                current_player = server_symbol
+#main function for playing the game
+def play_game(sock: socket.socket, is_server: bool):
+    while True:
+        #innit new game board
+        board = Board()
 
-                while not game_over:
-                    if current_player == server_symbol:
-                        print_board(board)
-                        print("Your move (0-8):")
-                        move = int(input())
-                        board, game_over = handle_move(board, server_symbol, move)
-                        conn.sendall(board.encode())
-                    else:
-                        board = conn.recv(BUFFER_SIZE).decode()
-                        game_over = check_winner(board) != ""
+        #determine players
+        if is_server:
+            opponent_choice = sock.recv(1024).decode()
+            opponent_player = Player[opponent_choice]
+            my_player = Player.O if opponent_player == Player.X else Player.X
+        else:
+            #if instance is client, ask the user for their choice, and send it to the server
+            choice = input("Choose your player (X/O): ").upper()
+            while choice not in ["X", "O"]:
+                choice = input("Invalid choice. Choose your player (X/O): ").upper()
 
-                    current_player = client_symbol if current_player == server_symbol else server_symbol
+            my_player = Player[choice]
+            opponent_player = Player.O if my_player == Player.X else Player.X
+            sock.sendall(choice.encode())
 
-                replay = input("Do you want to play again? (yes/no): ")
-                conn.sendall(replay.encode())  # Send the replay decision to the client
-                if replay.lower() == "yes":
-                    board = " " * 9
-                    conn.sendall(board.encode())  # Send the reset board to the client
+        #setting the initial turn, server always goes first because client chose their player first
+        my_turn = is_server
+
+        #Game loop - continues until a player wins or the board is full (draw)
+        while True:
+            if my_turn:
+                #if it is the player's turn, ask for their move, and send it to the server
+                print("Your turn:")
+                move = input("Enter your move (row col): ")
+                row, col = map(int, move.split())
+                #if move is valid, send the move to the opponent or print invalid move 
+                if board.make_move(row, col, my_player):
+                    sock.sendall(move.encode())
+                    print(board)
+                    #check if current player has won, or if the board is full (draw)
+                    if board.is_winner(my_player):
+                        print("You win!")
+                        break
+                    elif board.is_full():
+                        print("It's a draw!")
+                        break
+                    #switch turns
+                    my_turn = not my_turn
                 else:
-                    break
-
-
-def client(address: str, port: int) -> None:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((address, port))
-        print("Connected to the server.")
-
-        # Receive the symbol from the server
-        player = sock.recv(BUFFER_SIZE).decode()
-        opponent = "O" if player == "X" else "X"
-        print(f"Your symbol is {player}")
-
-        replay = "yes"
-        while replay.lower() == "yes":
-            game_over = False
-            current_player = opponent
-
-            while not game_over:
-                board = sock.recv(BUFFER_SIZE).decode()
-
-                if board == "end":
-                    break
-
-                print_board(board)
-                game_over = check_winner(board) != ""
-
-                if not game_over:
-                    if current_player == player:
-                        print("Your turn")
-                        move = input("Your move (0-8): ")
-                        board, game_over = handle_move(board, player, int(move))
-                        sock.sendall(board.encode())
-                    else:
-                        print("Waiting for opponent's move...")
-                else:
-                    print("Game over")
-
-                current_player = opponent if current_player == player else player
-
-            replay = input("Do you want to play again? (yes/no): ").lower()
-            sock.sendall(replay.encode())
-
-            if replay == "yes":
-                board = sock.recv(BUFFER_SIZE).decode()  # Receive the reset board from the server
+                    print("Invalid move. Try again.")
             else:
-                break
+                #if it is the opponent's turn, wait for their move
+                print("Waiting for the opponent's move...")
+                move = sock.recv(1024).decode()
+                row, col = map(int, move.split())
+                board.make_move(row, col, opponent_player)
+                #display the updated board
+                print(board)
+                #check if opponent has won
+                if board.is_winner(opponent_player):
+                    print("You lose!")
+                    break
+                #switch turns
+                my_turn = not my_turn
 
+        print("Game over.")
 
+        # Replay feature
+        if is_server:
+            # If the instance is the server, ask the player if they want to play again
+            play_again_server = input("Do you want to play again? (y/n): ")
 
+            # Send the server's decision to the client
+            sock.sendall(play_again_server.encode())
 
+            # Receive the client's decision about playing again
+            play_again_client = sock.recv(1024).decode()
+        else:
+            # If the instance is the client, receive the server's decision about playing again
+            play_again_server = sock.recv(1024).decode()
 
+            # Ask the client player if they want to play again
+            play_again_client = input("Do you want to play again? (y/n): ")
 
+            # Send the client's decision to the server
+            sock.sendall(play_again_client.encode())
 
+        # If both players do not want to play again, close the connection and exit the loop
+        if play_again_server.lower() != 'y' or play_again_client.lower() != 'y':
+            print("Closing connection. Client and/or Server did not want to play again. Goodbye!")
+            break
 
-def main():
-    parser = argparse.ArgumentParser(description="Networked Tic Tac Toe")
+if __name__ == "__main__":
+    #create argument parser
+    parser = argparse.ArgumentParser(description="Tic-Tac-Toe Client/Server")
+    #add mutually exclusive group for server and client
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-s", "--server", nargs='?', const=DEFAULT_PORT, metavar="PORT", type=int, help="Run as server, listening on PORT (defaults to 5131)")
-    group.add_argument("-c", "--client", action="store_true", help="Run as client, connecting to server")
-    parser.add_argument("--host", default="localhost", metavar="HOST", help="Specify the host to connect as a client (defaults to localhost)")
-    parser.add_argument("-p", "--port", default=DEFAULT_PORT, metavar="PORT", type=int, help="Specify the port to connect or listen (defaults to 5131)")
+    #add the server to the group
+    group.add_argument("-s", "--server", action="store_true", help="Run as server")
+    #add the client to the group, expect a host as an additional argument
+    group.add_argument("-c", "--client", metavar="HOST", help="Run as client and connect to server with the given host")
+    #add the default argument to the parser with a default port of 5131
+    parser.add_argument("port", nargs="?", type=int, default=DEFAULT_PORT, help="Port to use (default: 5131)")
 
     args = parser.parse_args()
 
+    #if server arg provided, run server function
     if args.server:
         server(args.port)
+    #if client arg provided, run client function
+    elif args.client:
+        client(args.client, args.port)
+    #if neither arg provided, print error message
     else:
-        client(args.host, args.port)
-
-if __name__ == "__main__":
-    main()
+        print("Invalid arguments. Please use -s or -c to specify server or client mode.")
+    
 
